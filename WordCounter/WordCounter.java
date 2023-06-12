3b323c87f7ab6926658d8b61f48ef278144b6ce0
Kobeeeef <83244503+Kobeeeef@users.noreply.github.com>
2023-06-12 11:15:55 -0700

    Update WordCounter.java

--- WordCounter/WordCounter.java
+++ WordCounter/WordCounter.java
@@ -5,48 +5,55 @@ import java.util.ArrayList;
 
 public class WordCounter {
     public static void main(String[] args) {
-        Dictionary dic  = new Dictionary();
+        // Author: Adem Can
+        
+        Dictionary dic = new Dictionary(); // Create a dictionary object
         System.out.println("Word Counter");
-        String readFile = input("Please provide the file you would like me to count");
-        ArrayList<String> errors = new ArrayList<String>();
-        int wordCount = 0;
-        int errorCount  = 0;
+        String readFile = input("Please provide the file you would like me to count"); // Prompt user for file name
+        ArrayList<String> errors = new ArrayList<String>(); // Create an ArrayList to store errors
+        int wordCount = 0; // Variable to keep track of word count
+        int errorCount  = 0; // Variable to keep track of error count
+        
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(readFile));
+            BufferedReader reader = new BufferedReader(new FileReader(readFile)); // Open the file for reading
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] words = line.split(" ");
-
-                for(String s :words){
-                    if(s.length()<1){
-                        continue;
-                    }
-                    try {
-                        Integer.parseInt(s);
-                        continue;
-                    } catch (Exception e) {
-                        
+                String[] words = line.split(" "); // Split the line into an array of words
+                
+                for (String s : words) {
+                    if (s.length() < 1) {
+                        continue; // Skip empty words
                     }
                     
-
-                    if(dic.Check(s.toLowerCase()) == false){
-                        errorCount +=1;
-                        errors.add(s);
+                    try {
+                        Integer.parseInt(s);
+                        continue; // Skip words that can be parsed as integers
+                    } catch (Exception e) {
+                        // Word is not an integer
+                    }
+                    
+                    if (dic.Check(s.toLowerCase()) == false) {
+                        errorCount += 1; // Increment error count
+                        errors.add(s); // Add the invalid word to the errors list
                     }
                 }
-                wordCount += words.length;
+                
+                wordCount += words.length; // Increment word count by the number of words in the line
             }
-            reader.close();
+            
+            reader.close(); // Close the file reader
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Print any exceptions that occur during file reading
         }
-        System.out.println("You have " + wordCount + " words in your file, there are " + errorCount +" errors;");
         
-        if(input("Would you like to see the errors (y/n)").toLowerCase().equals("y")){
+        System.out.println("You have " + wordCount + " words in your file, there are " + errorCount + " errors;");
+        
+        if (input("Would you like to see the errors (y/n)").toLowerCase().equals("y")) {
             System.out.println("The errors are " + errors.toString());
         }
     }
 
+    // Prompt the user for input
     private static String input(String prompt) {
         Scanner read = new Scanner(System.in);
         System.out.println(prompt);
